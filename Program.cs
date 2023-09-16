22d4acf0eeb0c1f2a9531e4f6f4d22e689dc23b9
«Daria» <Spirina_0136@mail.ru>
2023-09-16 23:00:22 +0700

    commit1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+if (numberA < numberB)
+{
+    Console.WriteLine($"Наибольшее число {numberB}, а наименьшее число {numberA}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число {numberA}, а наименьшее число {numberB}");
+}
